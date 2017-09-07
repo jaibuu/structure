8cfbc6790b17c7102ed3a1f2591570fc2f657924
Jairo Narvaez <jaibuu+git@gmail.com>
2017-09-06 22:49:08 -0500

    install

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -771,3 +771,4 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/loc_structure_dd.inc';
 }
 // </DDSETTINGS>
+$settings['install_profile'] = 'standard';
